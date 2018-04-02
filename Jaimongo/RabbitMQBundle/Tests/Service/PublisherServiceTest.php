028035a30953677e326c4ff63c8f1094ef1c6f4a
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2018-04-01 19:11:12 -0500

    PublisherServiceTest

---
+++ Jaimongo/RabbitMQBundle/Tests/Service/PublisherServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author <a href="mailto:mr.ljaime@gmail.com">José Jaime Ramírez Calvo</a>
+ * @version 1
+ * @since 2018-04-01
+ */
+
+namespace RabbitMQBundle\Services;
+
+/**
+ * Class PublisherServiceTest
+ * @package RabbitMQBundle\Services
+ * TODO:
+ */
+class PublisherServiceTest
+{
+
+}
